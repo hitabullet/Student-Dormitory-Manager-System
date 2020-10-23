ba3834b7e7abd90a1792bf5f272000a55b9715dc
zhoujun <1715607986@qq.com>
2020-10-23 15:10:42 +0800

    last commit

--- dormitory-back/src/test/java/cn/edu/ncc/zsh/ZshApplicationTests.java
+++ dormitory-back/src/test/java/cn/edu/ncc/zsh/ZshApplicationTests.java
@@ -15,17 +15,5 @@ class ZshApplicationTests {
     @Test
     void contextLoads() {
     }
-    @Autowired
-    private JavaMailSender javaMailSender;
-    @Test
-    public void text1() {
-//      第一种
-        SimpleMailMessage msg = new SimpleMailMessage();
-        msg.setFrom("1729656686@qq.com");//发送者
-        msg.setTo("\n" +
-                "1729656686@qq.com");//接收者
-        msg.setSubject("邮箱标题");//标题
-        msg.setText("https://www.cnblogs.com/lin02/");//内容
-        javaMailSender.send(msg);
-    }
+
 }
